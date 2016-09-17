367a5e59c709892a96b08473b4ab3f596ef3d13d
Master92 <Nils.Friedchen@googlemail.com>
2016-09-17 15:02:43 +0200

    Changed license to GPLv2 only

--- src/GlightsAboutDialog.cpp
+++ src/GlightsAboutDialog.cpp
@@ -2,8 +2,7 @@
 //
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
+//  the Free Software Foundation version 2.
 //
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -21,7 +20,7 @@ GlightsAboutDialog::GlightsAboutDialog(Gtk::Window& parent) {
     this->set_name("GLights");
     this->set_version("0.1b");
     this->set_transient_for(parent);
-    this->set_license_type(Gtk::LICENSE_GPL_2_0);
+    this->set_license_type(Gtk::LICENSE_GPL_2_0_ONLY);
     this->set_copyright("Nils Friedchen");
     
     std::vector<Glib::ustring> authors;
